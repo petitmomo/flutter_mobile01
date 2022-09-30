5282e9058b21d5d0ea95f1ea51de752cc406e1c4
Momo <ba94.mamadou@gmail.com>
2022-09-30 13:06:46 +0000

    BottomNavigationBar et PageView

---
+++ lib/home_screen.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import 'screens/list_view.dart';
+import 'screens/message_view.dart';
+import 'screens/screen_view.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final PageController _pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("BottomNavigationBar"),
+        centerTitle: true,
+      ),
+      body: PageView(
+          onPageChanged: (index) {
+            setState(() => _currentIndex = index);
+          },
+          controller: _pageController,
+          children: const <Widget>[
+            ScreenView(),
+            ListViewAstags(),
+            MessageView(),
+          ]),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() => _currentIndex = index);
+          _pageController.jumpToPage(_currentIndex);
+        },
+        items: [
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Accueil",
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.list),
+            label: "Hastags",
+          ),
+                    const BottomNavigationBarItem(
+            icon: Icon(Icons.send),
+            label: "Message",
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-//import 'package:flutter_rating/flutter_rating.dart';
+import 'home_screen.dart';
 
-//import 'package:flutter_rating/flutter_rating.dart';
-//import 'package:flutter_rating_stars/flutter_rating_stars.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,105 +15,10 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData.dark(),
       themeMode: ThemeMode.light,
       title: "Cous Flutter",
-      home: const HomePage(),
+      home:  HomeScreen(),
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
-  @override
-  _HomePageState createState() => _HomePageState();
-}
 
-class _HomePageState extends State<HomePage> {
-  String info = "";
-  bool change = true;
-  String a = "images/5.png";
-  String b = "images/7.png";
-  String c = "images/9.png";
-  String d = "images/10.png";
-  void onsubmit(String value){
-    setState(() {
-          info=value;
-        });
-  }
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        backgroundColor: Colors.blue,
-        title: Text(
-          "GestureDetector or inkwell",
-          style: TextStyle(
-            fontSize: 25,
-            color: Colors.black,
-          ),
-        ),
-        actions: [
-          const Icon(Icons.computer_sharp, color: Colors.black, size: 30)
-        ],
-      ),
-      body: Center(
-        child: Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Text('Mouvement de la souris : $info',style: TextStyle(
-                color: Colors.blue, fontSize: 22, fontWeight: FontWeight.bold
-              ),
-              ),
-              Row(
-                children: [
-                  Image.asset(change?a:b, width: 170, height: 170),
-                  Image.asset(change?c:d,width: 170, height: 170),
 
-                ],
-              ),
-              GestureDetector(
-                onDoubleTap: (){
-                  if (change=true) {
-                    change=true;
-                    onsubmit("Deux cliques");
-                  } else {
-                    change=false;
-                  }
-                },
-                onTap: (){
-                  if (change=true) {
-                     change=false;
-                     onsubmit("Un clique");
-                  } else {
-                    change=true;
-                  }
-                },
-                onLongPress: (){
-                  onsubmit("Long clique");
-                },
-                onTapCancel: (){
-                  onsubmit("Clique annaulé");
-                },
-                child: Container(
-                  width: 150,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlueAccent,
-                    borderRadius: BorderRadius.circular(15)
-                  ),
-                  child: Container(
-                    alignment: Alignment.center,
-                     child: const Text("Validation", style: TextStyle(
-                    color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold,
-                  )),
-                  )
-                 
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
