f41ba9590f4f5b3d4134f287c8f3d767cc7256c3
Momo <ba94.mamadou@gmail.com>
2022-09-30 18:04:22 +0000

    add ListViewBuider

--- lib/screens/list_view.dart
+++ lib/screens/list_view.dart
@@ -1,12 +1,51 @@
 import 'package:flutter/material.dart';
 
+
+const List<Map<String, dynamic>> listHasTags = [
+  {
+    "name":"#flutter",
+  },
+   {
+    "name":"#dart",
+  },
+   {
+    "name":"#pyhon",
+  },
+   {
+    "name":"javaScript",
+  },
+];
 class ListViewAstags extends StatelessWidget {
   const ListViewAstags({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text("Liste astags"),
+    return listHasTags.isEmpty? Center(
+      child: const Text("Pas de hastags"),
+    )
+    : ListView.builder(
+        itemCount: listHasTags.length,
+        itemBuilder: (context, index) {
+        return Dismissible(
+          key: Key(index.toString()),
+          onDismissed: (direction) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text("$index supprimé"),
+              ),
+            );
+          },
+          background: Container(
+            color: Colors.red,
+          ),
+          child: Card(
+            child: ListTile(
+              title: Text(listHasTags[index]["name"],
+            ),
+          ),
+        ),
+        );
+      },
     );
   }
 }
